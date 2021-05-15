682c5a77b374ac9d8f1e05b6ac20229d591a75c6
Murilo Gomes <murilo.goomes@hotmail.com>
2021-05-14 21:38:44 -0300

    feat: create CRUD for alunos

---
+++ app/Controllers/Http/AlunosController.ts
@@ -0,0 +1,42 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Aluno from 'App/Models/Aluno'
+
+export default class AlunosController {
+  public async index({}: HttpContextContract) {
+    const alunos = await Aluno.all()
+
+    return alunos
+  }
+
+  public async store({ request }: HttpContextContract) {
+    const data = request.only(['nome', 'email', 'matricula', 'nascimento'])
+    const aluno = await Aluno.create(data)
+
+    return aluno
+  }
+
+  public async show({ params }: HttpContextContract) {
+    const aluno = await Aluno.findOrFail(params.id)
+
+    return aluno
+  }
+
+  public async update({ request, params }: HttpContextContract) {
+    const aluno = await Aluno.findOrFail(params.id)
+    const data = request.only(['nome', 'email', 'matricula', 'nascimento'])
+
+    aluno.merge(data)
+
+    await aluno.save()
+
+    return aluno
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const aluno = await Aluno.findOrFail(params.id)
+
+    await aluno.delete()
+
+    return { message: 'Aluno deletado com sucesso' }
+  }
+}
---
+++ app/Models/Aluno.ts
@@ -0,0 +1,25 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Aluno extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @column()
+  public nome: string
+
+  @column()
+  public email: string
+
+  @column()
+  public matricula: string
+
+  @column()
+  public nascimento: Date
+}
